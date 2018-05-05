b51ae671ef2c3484929d544d1f01cf08b9634171
SuwakoMmh <SuwakoMmh@users.noreply.github.com>
2018-05-05 03:18:59 +0200

    Fixing /restart

--- main.py
+++ main.py
@@ -69,10 +69,10 @@ async def on_message(message):
 					await client.send_message(message.author, "```FAILED```")
 			#/// RESTART
 			elif str(message.content) == "/restart" :
-				async client.logout()
+				client.logout()
 			elif str(message.content) == "/restart py" :
 				await client.send_message(message.channel, message.author.mention() + ", Le module Python va redémarrer...")
-				async client.logout()
+				client.logout()
 
 
 
