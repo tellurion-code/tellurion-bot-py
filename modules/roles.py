bb7d390e093d06cab733d582ccfd62ff64c90091
Suwako Moriya <suwako@moriya.zapto.org>
2018-05-19 20:50:06 +0200

    Fixing a small typo in roles module

--- modules/roles.py
+++ modules/roles.py
@@ -6,7 +6,7 @@ async def AddRole(client, message) :
 		if len(args) > 2 :
 			await client.send_message(message.channel, message.author.mention + "\n ```\nusage :\n/roles list(e)\n/roles <nom du rôle>```")
 		if args[1] == "liste" or args[1] == "list" :
-			response = "```\nListe des roles disponnibles :\n"
+			response = "```\nListe des roles disponibles :\n"
 			for nom, id in settings.roles.RoleList.items() :
 				response += nom + "\n"
 			await client.send_message(message.channel, message.author.mention + response + "\n```")
@@ -16,9 +16,11 @@ async def AddRole(client, message) :
 				if discord.utils.get(message.server.roles, id=settings.roles.RoleList[args[1]]) in message.author.roles :
 					await client.remove_roles(message.author, discord.utils.get(message.server.roles, id=settings.roles.RoleList[args[1]]))
 					await client.send_message(message.channel, message.author.mention + ", vous avez perdu le rôle " + args[1] +".")
+					await client.delete_message(message)
 				else :
 					await client.add_roles(message.author, discord.utils.get(message.server.roles, id=settings.roles.RoleList[args[1]]))
 					await client.send_message(message.channel, message.author.mention + ", vous avez reçu le rôle " + args[1] +".")
+					await client.delete_message(message)
 			except KeyError:
 				await client.delete_message(message)
 				await client.send_message(message.channel, message.author.mention + ", ce rôle n'est pas disponnible à l'auto-attribution. Vous pouvez avoir la liste des rôles disponnible grâce à la commande /roles liste .")
