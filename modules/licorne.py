8471eebbb2e0a477dbc00f760f12418c702b83fd
Suwako Moriya <suwako@moriya.zapto.org>
2018-05-21 01:43:11 +0200

    Fixing a small bug (typo ?)

--- modules/licorne.py
+++ modules/licorne.py
@@ -1,5 +1,5 @@
 import utils.perms
 import settings.licorne
 async def Licorne(client, message) :
-	if message.content == "/licorne" and utils.perms.hasrole(message.author, settings.licorne.licorneAuth) :
+	if message.content == "/licorne" and await utils.perms.hasrole(message.author, settings.licorne.licorneAuth) :
 		a = 7/0
