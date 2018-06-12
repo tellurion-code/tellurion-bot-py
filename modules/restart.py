75688d045fcde3c59d95b9c7305a2fcd5a3fcbc1
Alix Eisenhardt <alix.eisenhardt@gmail.com>
2018-06-12 14:25:43 +0200

    add : delete '/restart' massage

--- modules/restart.py
+++ modules/restart.py
@@ -3,6 +3,7 @@ import utils.perms
 async def restart_py(client, message):
 	if (not (message.author == client.user)) and (message.content.startswith("/restart") and await utils.perms.hasrole(message.author, settings.restart.restartAuth)):
 		if str(message.content) == "/restart" :
+			await client.delete_message(message)
 			await client.logout()
 		elif str(message.content) == "/restart py" :
 			await client.delete_message(message)
