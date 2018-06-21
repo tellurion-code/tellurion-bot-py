24cd92042bf1eec66e1c0458e7a4a93c8181a082
Suwako Moriya <suwako@moriya.zapto.org>
2018-06-21 19:59:58 +0200

    Adding descriptions to roles

--- modules/roles.py
+++ modules/roles.py
@@ -7,18 +7,18 @@ async def AddRole(client, message) :
 			await client.send_message(message.channel, message.author.mention + "\n ```\nusage :\n/roles list(e)\n/roles <nom du rôle>```")
 		elif args[1] == "liste" or args[1] == "list" :
 			response = "```\nListe des roles disponibles :\n"
-			for nom, id in settings.roles.RoleList.items() :
-				response += nom + "\n"
+			for nom, idgrp in settings.roles.RoleList.items() :
+				response += nom + "  =>  " + idgrp[1] + "\n"
 			await client.send_message(message.channel, message.author.mention + response + "\n```")
 			await client.delete_message(message)
 		else :
 			try :
 				if discord.utils.get(message.server.roles, id=settings.roles.RoleList[args[1]]) in message.author.roles :
-					await client.remove_roles(message.author, discord.utils.get(message.server.roles, id=settings.roles.RoleList[args[1]]))
+					await client.remove_roles(message.author, discord.utils.get(message.server.roles, id=settings.roles.RoleList[args[1]][0]))
 					await client.send_message(message.channel, message.author.mention + ", vous avez perdu le rôle " + args[1] +".")
 					await client.delete_message(message)
 				else :
-					await client.add_roles(message.author, discord.utils.get(message.server.roles, id=settings.roles.RoleList[args[1]]))
+					await client.add_roles(message.author, discord.utils.get(message.server.roles, id=settings.roles.RoleList[args[1]][0]))
 					await client.send_message(message.channel, message.author.mention + ", vous avez reçu le rôle " + args[1] +".")
 					await client.delete_message(message)
 			except KeyError:
