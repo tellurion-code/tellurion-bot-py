1045e8c9b00ef2c9e14effdda0830508a92c7911
Alix Eisenhardt <alix.eisenhardt@gmail.com>
2018-06-25 16:00:46 +0200

    merge alix into testing

--- modules/ekiller.py
+++ modules/ekiller.py
@@ -74,7 +74,7 @@ async def start(client, message, ekiller):
         word = random.choice(ekiller.words)
         embed = discord.Embed(title="E-KILLER", description="Votre cible est : " + players[i+1].nick + "\nLe mot est : `" + word + "`\n\nBonne chance à vous.", color=0x0000ff)
         await client.send_message(players[i], embed=embed)
-    await client.send_message(message.channel, "La partie de E-Killer a été lancée.")
+    await client.send_message(message.channel, "La partie de E-Killer a été lancée.\nRécapitulatif :")
     await print_players(client, message, ekiller)
     await print_words(client, message, ekiller)
     await client.send_message(message.channel, "Bonne chance à tous !")
