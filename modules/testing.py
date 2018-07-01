9e382ae81a147b75586f817b5de6501d9175e51a
Suwako Moriya <suwako@moriya.zapto.org>
2018-07-01 14:38:14 +0200

    [testing module] Syntax error fix

--- modules/testing.py
+++ modules/testing.py
@@ -30,5 +30,5 @@ async def testsHandler(client, message, globs, locs) :
             await client.logout()
     if message.content.startswith("/testing exec ") and utils.perms.hasrole(message.author, '1'):
         msg=message.content
-        if msg.split(' ')[2] in ['testing', 'master'] and msg.split(' '))[2] == pygit2.Repository('.').head.shorthand:
+        if msg.split(' ')[2] in ['testing', 'master'] and msg.split(' ')[2] == pygit2.Repository('.').head.shorthand:
             exec(msg[15 + len(msg.split(' ')[2])::])
