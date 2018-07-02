18952bafc3d525a148dde69cc3e7f1596559c7a9
Suwako Moriya <suwako@moriya.zapto.org>
2018-07-02 14:41:56 +0200

    [avalon module] coding style fix

--- modules/avalon.py
+++ modules/avalon.py
@@ -7,9 +7,9 @@ async def commandHandler(client, message, avalonGame):
         if avalonGame.state=='lobby':
     #     -Join command-
             if message.content=='/avalon join':
-                if not message.author.id in avalonGame.playersid:
-                    if not len(avalonGame.playersid)>=12:
-                        avalonGame.playersid.append(message.author.id)
+                if not message.author in avalonGame.players:
+                    if not len(avalonGame.players)>=10:
+                        avalonGame.players.append(message.author)
                         await client.send_message(message.channel, message.author.mention + " a rejoint la partie.")
                     else:
                         await client.send_message(message.channel, message.author.mention + ", la partie est complète..")
@@ -17,16 +17,19 @@ async def commandHandler(client, message, avalonGame):
                     await client.send_message(message.channel, message.author.mention + ", vous êtes déjà dans la partie..")
 
     #     -Quit command-
-            if message.content=='/avalon quit' and avalonGame.state=='lobby':
-                if message.author.id in avalonGame.playersid :
-                    avalonGame.playersid.remove(message.author.id)
+            if message.content=='/avalon quit'
+                if message.author in avalonGame.players :
+                    avalonGame.players.remove(message.author)
                     await client.send_message(message.channel, message.author.mention + " a quitté la partie.")
                 else:
                     await client.send_message(message.channel, message.author.mention + ", vous n'êtes pas dans la partie..")
 
     #     -Player list command :
             if message.content=='/avalon players list':
-                await client.send_message(message.channel, "Liste des joueurs :\n```PYTHON\n{0}```".format(str(list(map(lambda x:utils.usertools.UserByID(x).name, avalonGame.playersid)))))
+                players=[]
+                for user in avalonGame.players :
+                    players.append(user.name)
+                await client.send_message(message.channel, "Liste des joueurs :\n```PYTHON\n{0}```".format(players)
 
     #     -Roles list command-
             if message.content=='/avalon roles list':
@@ -34,7 +37,7 @@ async def commandHandler(client, message, avalonGame):
 
     #     -Add Role command-
             if message.content.startswith('/avalon roles add'):
-                if len(message.content.split(' '))==3:
+                if len(message.content.split(' '))==4:
                     ans=""
                     for role in message.content.split(' ')[2].split(','):
                         if role in avalonGame.implemented_roles :
@@ -47,9 +50,10 @@ async def commandHandler(client, message, avalonGame):
 
     #     -Remove role command-
             if message.content.startswith('/avalon roles remove'):
-                if len(message.content.split(' '))==3:
+                args=message.content.split(' ')
+                if len(args)==3:
                     ans=""
-                    for role in message.content.split(' ')[2].split(','):
+                    for role in args[2].split(','):
                         if role in avalonGame.implemented_roles :
                             roles.remove(role)
                             ans += "{0} retiré\n".format(role)
@@ -62,6 +66,6 @@ async def commandHandler(client, message, avalonGame):
 class AvalonSave:
     def __init__(self):
         self.implemented_roles=['gentil', 'mechant', 'merlin', 'perceval', 'morgane', 'assassin', 'mordred']
-        self.playersid=[]
+        self.players=[]
         self.state='lobby' # Differents states : {'lobby':'Players are joining and choosing the roles', }
         self.roles=[]
