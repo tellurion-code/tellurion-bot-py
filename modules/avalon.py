d8d08890fa07684151b4ada458e250cadbd7a22e
Suwako Moriya <suwako@moriya.zapto.org>
2018-07-06 01:30:58 +0200

    [avalon module] Assassin broadcast

--- modules/avalon.py
+++ modules/avalon.py
@@ -408,6 +408,7 @@ class AvalonSave:
             if not self.actors[i]['role'] in ['mechant', 'assassin', 'mordred', 'morgane', 'oberon'] :
                 self.assassinlist.append(i)
                 await client.add_reaction(self.assassinmsg, self.emotes[i])
+        await client.send_message(self.statuschan, discord.Embed(title="AVALON", description="Les méchants vont débattre pour choisir celui qu'ils pensent être merlin. Que les gentils coupent leur micros."))
     async def assassinationCheck(self, client):
         if len(self.assassinkilllist) == 1 :
             if not self.assassinvalid:
