9898ee894bce24db3ae917b6b7bd578c55d71f90
Suwako Moriya <suwako@moriya.zapto.org>
2018-07-10 16:08:04 +0200

    [avalon module] Typeerror fix

--- modules/avalon.py
+++ modules/avalon.py
@@ -228,7 +228,11 @@ class AvalonSave:
             rolesstr+= " {0} `{1}` : `{2}`\n".format(self.emotes[self.actors.index(actor)], actor['user'].display_name + '#' + str(actor['user'].discriminator), actor['role'])
         if self.killed is not None:
             rolesstr+="L'assassin tue {0} qui était {1}.\n".format(" {0} `{1}`".format(self.emotes[self.killed], self.actors[self.killed]['user'].display_name + '#' + str(self.actors[self.killed]['user'].discriminator)), self.actors[self.killed]['role'])
-        if self.quests.count(False)==3 or self.votefailcount==5 or self.actors[self.killed]['role']=='merlin':
+            if self.actors[self.killed]['role']=='merlin':
+                rolesstr+="**Les méchants gagnent !**"
+            else:
+                rolesstr+="**Les gentils gagnent !**"
+        elif self.quests.count(False)==3 or self.votefailcount==5:
             rolesstr+="**Les méchants gagnent !**"
         elif self.quests.count(True)==3:
             rolesstr+="**Les gentils gagnent !**"
