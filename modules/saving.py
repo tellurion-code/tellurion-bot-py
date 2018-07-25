062c3e69d48b5a4f9e6ce5f1533f7226bf8189e7
Suwako Moriya <suwako@moriya.zapto.org>
2018-07-25 02:57:09 +0200

    [saving module] file not found error fix

--- modules/saving.py
+++ modules/saving.py
@@ -2,16 +2,15 @@ import pickle
 import os.path
 from subprocess import call
 def saveObject(object, objectname):
-    with open("tmp/" + objectname, "wb") as pickleFile:
+    with open("tmp/" + objectname + tmp, "wb") as pickleFile:
         pickler = pickle.Pickler(pickleFile)
         return pickler.dump(object)
-
+    call(['mv', "tmp/" + objectname + "tmp", "tmp/" + objectname])
 def loadObject(objectname):
     if saveExists(objectname):
-        with open("tmp/" + objectname + "tmp", "rb") as pickleFile:
+        with open("tmp/" + objectname, "rb") as pickleFile:
             unpickler = pickle.Unpickler(pickleFile)
             return unpickler.load()
-        call(['mv', "tmp/" + objectname + "tmp", "tmp/" + objectname])
 
 def saveExists(objectname):
     return os.path.isfile("tmp/" + objectname)
