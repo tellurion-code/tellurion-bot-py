5cb78975a2e93701153188d5387ad18f13091b1a
SuwakoMmh <SuwakoMmh@users.noreply.github.com>
2018-10-07 14:43:10 +0200

    Add clash-of-coders role to auto-attribution list

--- settings/roles.py
+++ settings/roles.py
@@ -1,4 +1,5 @@
 enabled = True
 RoleList = {
-    "suspect":["435559220860157952", "Permet d'être notifié pour les mini-jeux"]
+    "suspect":["435559220860157952", "Permet d'être notifié pour les mini-jeux"],
+    "clash-of-coders":["496372436967882774", "Permet d'être notifié pour les parties de clash-of-code"]
             }
