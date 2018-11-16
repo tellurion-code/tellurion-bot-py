8aac42f3d858f2ac8138cf23e8c8b47b2b1570ff
SuwakoMmh <SuwakoMmh@users.noreply.github.com>
2018-11-16 22:55:29 +0100

    [roles module] added \n suffix in description

--- settings/roles.py
+++ settings/roles.py
@@ -2,5 +2,5 @@ enabled = True
 RoleList = {
     "suspect":["435559220860157952", "Permet d'être notifié pour les mini-jeux\n"],
     "clash-of-coders":["496372436967882774", "Permet d'être notifié pour les parties de clash-of-code\n"],
-    "civ-e-lization":["513098739515392022","Donne accès aux salons liés aux parties de Civilization"]
+    "civ-e-lization":["513098739515392022","Donne accès aux salons liés aux parties de Civilization\n"]
             }
