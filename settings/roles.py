7564196ecc7f0fa9b749ad8575e3c0dce258b7e3
SuwakoMmh <SuwakoMmh@users.noreply.github.com>
2018-11-16 22:53:54 +0100

    [roles module] Added Civ-E-lization in settings

--- settings/roles.py
+++ settings/roles.py
@@ -1,5 +1,6 @@
 enabled = True
 RoleList = {
     "suspect":["435559220860157952", "Permet d'être notifié pour les mini-jeux\n"],
-    "clash-of-coders":["496372436967882774", "Permet d'être notifié pour les parties de clash-of-code\n"]
+    "clash-of-coders":["496372436967882774", "Permet d'être notifié pour les parties de clash-of-code\n"],
+    "civ-e-lization":["513098739515392022","Donne accès aux salons liés aux parties de Civilization"]
             }
