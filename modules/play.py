aff40d1622f40d01a0f6b10d638b1c20aacb1717
Alix Eisenhardt <Alix-Eisenhardt@users.noreply.github.com>
2019-02-27 16:22:58 +0100

    Update play.py

--- modules/play.py
+++ modules/play.py
@@ -40,7 +40,7 @@ class MainClass():
                 await message.channel.send(message.author.mention + ", ça à l'air de fonctionner, ici aussi.")
                 voice.play(discord.FFmpegPCMAudio("/home/epenser/epenser-bot/soundbox/for-the-damaged-coda.mp3"))
                 while voice.is_playing():
-                    pass
+                    await asyncio.sleep(1)
                 await message.channel.send(message.author.mention + ", ça à l'air de fonctionner, ici aussi. bis")
                 if voice.is_connected():
                     await voice.disconnect()
