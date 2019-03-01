5d8d68df4c7e609436e477af2d8ae6213b8474f4
Pierre CHAMINADE <pierre.chaminade@esme.fr>
2019-03-01 01:56:45 +0100

    [rtfgd] fix2

--- modules/rtfgd.py
+++ modules/rtfgd.py
@@ -1,6 +1,7 @@
 
 import discord
 import asyncio
+import random
 class MainClass():
     def __init__(self, client, modules, owners, prefix):
         self.client = client
@@ -11,11 +12,21 @@ class MainClass():
         self.command="%srtfgd"%self.prefix #command prefix (can be empty to catch every single messages)
 
         self.name="rtfgd"
-        self.description="ceci t'explique pourquoi tu es une merde and rtfgd !"
+        self.description="Module donnant des indications sur les Google Docs"
         self.interactive=True
         self.color=0x7289da
         self.help="""\
  Aucune fonction.
 """
+        self.meme = meme = [
+			"https://i.imgflip.com/27tjun.gif",
+			"https://cdn.discordapp.com/attachments/326742676672086018/431574829607419914/telecharge_25.jpg",
+			"https://cdn.discordapp.com/attachments/326742676672086018/431575352381538304/telecharge_26.jpg",
+			"https://cdn.discordapp.com/attachments/326742676672086018/431574292904280064/unknown.png",
+			"https://cdn.discordapp.com/attachments/326742676672086018/431573317539856394/image-1.png",
+			"https://cdn.discordapp.com/attachments/326742676672086018/431572693910028289/telecharge_19.jpg",
+			"https://cdn.discordapp.com/attachments/326742676672086018/431572366951448588/telecharge_18.jpg",
+			"https://cdn.discordapp.com/attachments/326742676672086018/431569158170345472/telecharge_11.jpg"
+            ]
     async def on_message(self, message):
-        await message.channel.send(embed=discord.Embed(title="rtfgd", color=self.color).set_image(url="https://cdn.discordapp.com/attachments/326742676672086018/431574829607419914/telecharge_25.jpg"))
+        await message.channel.send(message.mentions, embed=discord.Embed(title="Read da fu**ing GOOGLE DOCS {1}! (╯°□°）╯︵ ┻━┻", color=self.color).set_image(url=random.choice(self.meme)))
