7dc65df65b87c4c4840194564ccf48f5081783cb
Suwako Moriya <suwako@moriya.zapto.org>
2019-03-01 05:02:58 +0100

    [rtfgd module] Replace tabs with space

--- modules/rtfgd.py
+++ modules/rtfgd.py
@@ -20,18 +20,18 @@ class MainClass():
  => Incite les personnes mentionnées à lire les Google Docs
 """
         self.meme = [
-			"https://i.imgflip.com/27tjun.gif",
-			"https://cdn.discordapp.com/attachments/326742676672086018/431574829607419914/telecharge_25.jpg",
-			"https://cdn.discordapp.com/attachments/326742676672086018/431575352381538304/telecharge_26.jpg",
-			"https://cdn.discordapp.com/attachments/326742676672086018/431574292904280064/unknown.png",
-			"https://cdn.discordapp.com/attachments/326742676672086018/431573317539856394/image-1.png",
-			"https://cdn.discordapp.com/attachments/326742676672086018/431572693910028289/telecharge_19.jpg",
-			"https://cdn.discordapp.com/attachments/326742676672086018/431572366951448588/telecharge_18.jpg",
-			"https://cdn.discordapp.com/attachments/326742676672086018/431569158170345472/telecharge_11.jpg"
+            "https://i.imgflip.com/27tjun.gif",
+            "https://cdn.discordapp.com/attachments/326742676672086018/431574829607419914/telecharge_25.jpg",
+            "https://cdn.discordapp.com/attachments/326742676672086018/431575352381538304/telecharge_26.jpg",
+            "https://cdn.discordapp.com/attachments/326742676672086018/431574292904280064/unknown.png",
+            "https://cdn.discordapp.com/attachments/326742676672086018/431573317539856394/image-1.png",
+            "https://cdn.discordapp.com/attachments/326742676672086018/431572693910028289/telecharge_19.jpg",
+            "https://cdn.discordapp.com/attachments/326742676672086018/431572366951448588/telecharge_18.jpg",
+            "https://cdn.discordapp.com/attachments/326742676672086018/431569158170345472/telecharge_11.jpg"
             ]
     async def on_message(self, message):
         await message.channel.send(" ".join(member.mention for member in message.mentions), embed=discord.Embed(title="Read da fu**ing GOOGLE DOCS ! (╯°□°）╯︵ ┻━┻", color=self.color).set_image(url=random.choice(self.meme)))
         try:
-		await message.delete()
-	except:
-		pass
+        await message.delete()
+    except:
+        pass
