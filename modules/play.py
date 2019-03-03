95998af6c5062d763d815821fe287eeda38cd2b0
Fomys <fomys@gmx.us>
2019-03-03 08:35:38 +0100

    Update play.py

--- modules/play.py
+++ modules/play.py
@@ -29,43 +29,46 @@ class MainClass(BaseClass):
             'pillar-men-theme',
             'ba-dum-tss'
         ]
+        self.voice = None
 
-    async def com_list(self, message, args, kwargs):
-        await message.channel.send(embed=discord.Embed(title="PLAY - Soundboard", description='\n'.join(
-            [str(i) + " : " + name for i, name in enumerate(self.musics)]), color=self.color))
 
-    async def command(self, message, args, kwargs):
-        try:
-            number = int(args[0])
-        except ValueError:
-            await message.channel.send("fail to parse int")
-        else:
-            if number in range(len(self.musics)):
-                await message.channel.send("..")
-                if not self.voice:
-                    await message.channel.send("ùù")
-                    self.voice = True
-                    self.voice = await message.author.voice.channel.connect()
-                    try:
-                        await message.delete()
-                    except discord.Forbidden:
-                        pass
-                    except discord.HTTPException:
-                        pass
-                    await message.channel.send("hhui int")
-                    self.voice.play(discord.PCMVolumeTransformer(
-                        discord.FFmpegPCMAudio("assets/" + self.musics[number] + ".mp3"), volume=0.1))
-                    await message.channel.send("lkjhghjh")
-                    while self.voice.is_playing():
-                        await asyncio.sleep(1)
-                    if self.voice and self.voice.is_connected():
-                        await self.voice.disconnect()
-                        self.voice = None
-            else:
-                await message.channel.send(message.author.mention + ", Veuillez préciser un nombre valide.")
+async def com_list(self, message, args, kwargs):
+    await message.channel.send(embed=discord.Embed(title="PLAY - Soundboard", description='\n'.join(
+        [str(i) + " : " + name for i, name in enumerate(self.musics)]), color=self.color))
+
+
+async def command(self, message, args, kwargs):
+    try:
+        number = int(args[0])
+    except ValueError:
+        await message.channel.send("fail to parse int")
+    else:
+        if number in range(len(self.musics)):
+            await message.channel.send("..")
+            if not self.voice:
+                await message.channel.send("ùù")
+                self.voice = True
+                self.voice = await message.author.voice.channel.connect()
                 try:
                     await message.delete()
                 except discord.Forbidden:
                     pass
                 except discord.HTTPException:
                     pass
+                await message.channel.send("hhui int")
+                self.voice.play(discord.PCMVolumeTransformer(
+                    discord.FFmpegPCMAudio("assets/" + self.musics[number] + ".mp3"), volume=0.1))
+                await message.channel.send("lkjhghjh")
+                while self.voice.is_playing():
+                    await asyncio.sleep(1)
+                if self.voice and self.voice.is_connected():
+                    await self.voice.disconnect()
+                    self.voice = None
+        else:
+            await message.channel.send(message.author.mention + ", Veuillez préciser un nombre valide.")
+            try:
+                await message.delete()
+            except discord.Forbidden:
+                pass
+            except discord.HTTPException:
+                pass
