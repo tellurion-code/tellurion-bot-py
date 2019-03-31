89edc3afaf251a1d9c2e285246e945572fafb37a
SuwakoMmh <SuwakoMmh@users.noreply.github.com>
2019-03-31 19:41:37 +0200

    Update roles.py

--- modules/roles.py
+++ modules/roles.py
@@ -22,9 +22,7 @@ class MainClass:
  => Vous donne ou vous retire le rôle donné
 """
         self.RoleList = {
-            "suspect": [435559220860157952, "Permet d'être notifié pour les mini-jeux"],
-            "clash-of-coders": [496372436967882774, "Permet d'être notifié pour les parties de clash-of-code"],
-            "civ-e-lization": [513098739515392022, "Donne accès aux salons liés aux parties de Civilization"],
+            "jeux": [435559220860157952, "Permet d'être notifié pour les mini-jeux/jeux"]
         }
 
     async def on_message(self, message):
