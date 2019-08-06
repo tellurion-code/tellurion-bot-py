3c868ae8f1268c02eeaa909cae239375d0b3fa3f
SuwakoMmh <SuwakoMmh@users.noreply.github.com>
2019-08-06 23:31:12 +0200

    Remove owners for master

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ class NikolaTesla(discord.Client):
         self.config = {
             "modules": ["modules"],
             "prefix": "%",
-            "owners": [281166473102098433, 318866596502306816, 118399702667493380]
+            "owners": [281166473102098433]
         }
         self.load_config()
         self.load_modules()
