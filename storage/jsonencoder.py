4c6a2d068506b3fd4c29adf76995a88fdcedfebe
Suwako Moriya <suwako@moriya.zapto.org>
2020-03-31 10:31:05 +0200

    Fix jsonencoder

--- storage/jsonencoder.py
+++ storage/jsonencoder.py
@@ -5,22 +5,12 @@ data_type = "__data_type"
 content = "__content"
 
 
-class Encoder(json.JSONEncoder):
-    def __init__(self):
-        super().__init__()
-        self.custom = {}
-
+class Encoder():
+    def __init__(self, *args, **kwargs):
+        self.custom = {Encoder:(lambda x:x, lambda x:x)}
+        self.JSONEncoder.custom = self.custom
     def register(self, type_, encode, decode):
         self.custom.update({type_: (encode, decode)})
-
-    def default(self, obj):
-        if isinstance(obj, tuple(self.custom.keys())):
-            return {data_type: f'{type(obj)}', content: self.custom[type(obj)][0](obj)}
-        if isinstance(obj, (datetime.datetime)):
-            return {data_type: 'datetime.datetime', 'iso': obj.isoformat()}
-        if isinstance(obj, (datetime.timedelta)):
-            return {data_type: 'datetime.timedelta', 'totalseconds': obj.total_seconds()}
-        return json.JSONEncoder.default(self, obj)
 
     def hook(self, dct):
         if data_type in dct:
@@ -32,3 +22,12 @@ class Encoder(json.JSONEncoder):
             elif dct[data_type] == "datetime.timedelta":
                 return datetime.timedelta(seconds=dct['totalseconds'])
         return dct
+    class JSONEncoder(json.JSONEncoder) :
+        def default(self, obj):
+            if isinstance(obj, tuple(self.custom.keys())):
+                return {data_type: f'{type(obj)}', content: self.custom[type(obj)][0](obj)}
+            if isinstance(obj, (datetime.datetime)):
+                return {data_type: 'datetime.datetime', 'iso': obj.isoformat()}
+            if isinstance(obj, (datetime.timedelta)):
+                return {data_type: 'datetime.timedelta', 'totalseconds': obj.total_seconds()}
+            return json.JSONEncoder.default(self, obj)
