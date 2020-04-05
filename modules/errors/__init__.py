fd3bef9240f02b5711eebc5dc6e4f0188049cf31
Le Codex <40335983+LeCodex@users.noreply.github.com>
2020-04-05 17:26:51 +0200

    Added emjoi functionnality to remove error messages early

--- modules/errors/__init__.py
+++ modules/errors/__init__.py
@@ -85,11 +85,19 @@ class MainClass(BaseClassPython):
             self.objects.save_object('errorsList', self.errorsList)
 
             # Wait 60 seconds and delete message
-            await asyncio.sleep(60)
+            #await asyncio.sleep(60)
             try:
-                channel = self.client.get_channel(msg_id["channel_id"])
-                delete_message = await channel.fetch_message(msg_id["msg_id"])
-                await delete_message.delete()
+                # channel = self.client.get_channel(msg_id["channel_id"])
+                # delete_message = await channel.fetch_message(msg_id["msg_id"])
+                # await delete_message.delete()
+                await message.add_reaction("🗑️")
+
+                try:
+                    reaction, user = await self.client.wait_for('reaction_add', timeout=60.0, check=lambda r, u: r.emoji == "🗑️" and not u.bot)
+                except asyncio.TimeoutError:
+                    await message.delete()
+                else:
+                    await reaction.message.delete()
             except:
                 raise
             finally:
