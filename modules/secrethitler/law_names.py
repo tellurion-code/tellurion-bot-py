d58cc539c9db50a9a9e0f0223a46c03d2ddaabf7
Le Codex <40335983+LeCodex@users.noreply.github.com>
2020-06-27 14:31:44 +0200

    [mod-scrt_hltr] Added Article

--- modules/secrethitler/law_names.py
+++ modules/secrethitler/law_names.py
@@ -1,6 +1,6 @@
 import random
 
-type = ("Mention", "Loi", "Projet", "Déclaration", "Réforme", "Campagne")
+type = ("Mention", "Loi", "Projet", "Déclaration", "Réforme", "Campagne", "Article")
 nouns_fascist = ("Censure", "Collectivisation", "Militarisation", "Relance", "Taxe", "Purge", "Privatisation", "Confiscation", "Revue", "Délibération")
 nouns_liberal = ("Libéralisation", "Congolexicomatisation", "Négociation", "Paix", "Médecine", "Démilitarisation", "Prudence", "Sollicitude", "Projection", "Protection")
 adjectives = ("agressive", "générale", "rapide", "locale", "douce", "intrinsèque", "crédible", "correcte", "exceptionnelle", "banale", "forte", "festive", "admirable", "absolue", "nationale", "équitable", "orthogonale", "approximative", "médiocre", "relative")
