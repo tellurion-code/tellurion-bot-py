a8c3a44a95d6acdc9f5c826d6cf88293ce88b824
Le Codex <40335983+LeCodex@users.noreply.github.com>
2020-07-20 19:07:19 +0200

    [mod-rctn_msg] Initial commit

---
+++ modules/reaction_message/__init__.py
@@ -0,0 +1,76 @@
+import discord
+
+from modules.base import BaseClassPython
+from modules.reaction_message.reaction_message import ReactionMessage
+
+import modules.reaction_message.globals as global_values
+global_values.init()
+
+
+class MainClass(BaseClassPython):
+    name = "ReactionMessage"
+    help = {
+        "description": "Module des messages de choix",
+        "commands": {
+            "`{prefix}{command} test`": "Envoies un messgae à réaction par défaut"
+        }
+    }
+    help_active = False
+    command_text = "reactionmessage"
+    color = global_values.color
+
+    def __init__(self, client):
+        super().__init__(client)
+        self.config["name"] = self.name
+        self.config["coommand_text"] = self.command_text
+        self.config["color"] = self.color
+        self.config["help_active"] = self.help_active
+        self.config["configured"] = True
+        # self.config.init({"help_active": True,
+        #     "color": globals.color,
+        #     "auth_everyone": True,
+        #     "authorized_roles": [],
+        #     "authorized_users": [],
+        #     "command_text": "avalon",
+        #     "configured": True
+        # })
+
+    async def com_test(self, message, args, kwargs):
+        async def effect(reactions):
+            await message.channel.send("Le choix **" + [str(x[0] + 1) for x in reactions.values()][0] + "** a été validé")
+
+        async def cond(reactions):
+            if message.author.id in reactions:
+                return len(reactions[message.author.id]) == 1
+
+        await ReactionMessage(
+            cond,
+            effect
+        ).send(
+            message.channel,
+            "Message de Test",
+            "Ce message a pour but de tester les messages avec des réactions interactives",
+            global_values.color,
+            ["Choix 1", "Choix 2", "Choix 3", "Choix 4", "Choix 5"]
+        )
+
+    async def on_reaction_add(self, reaction, user):
+        if not user.bot:
+            for message in global_values.reaction_messages:
+                if message.message.id == reaction.message.id:
+                    if reaction.emoji in message.number_emojis:
+                        if message.check(reaction, user):
+                            await message.add_reaction(reaction, user)
+                        else:
+                            await message.message.remove_reaction(reaction, user)
+                    else:
+                        await message.message.remove_reaction(reaction, user)
+
+    async def on_reaction_remove(self, reaction, user):
+        if not user.bot:
+            for message in global_values.reaction_messages:
+                if user.id in message.reactions:
+                    if reaction.emoji in message.number_emojis:
+                        if message.number_emojis.index(reaction.emoji) in message.reactions[user.id]:
+                            if message.check(reaction, user) and message.message.id == reaction.message.id:
+                                await message.remove_reaction(reaction, user)
---
+++ modules/reaction_message/reaction_message.py
@@ -0,0 +1,97 @@
+import discord
+
+import modules.reaction_message.globals as global_values
+
+
+class ReactionMessage:
+    def __init__(self, _cond, _effect, **kwargs):
+        self.check = kwargs["check"] if "check" in kwargs else lambda r, u: True
+        self.update_function = kwargs["update"] if "update" in kwargs else None
+        self.temporary = kwargs["temporary"] if "temporary" in kwargs else True
+        self.cond = _cond
+        self.effect = _effect
+        self.block = False
+        self.number_emojis = None
+        self.message = None
+
+        self.reactions = {}
+
+    # Envoie le choix
+    async def send(self, _channel, _title, _description, _color, _choices, **kwargs):
+        if len(_choices) == 0:
+            raise Exception("Le nombre de choix doit être supérieur à 0")
+
+        embed = discord.Embed(
+            title=_title,
+            description=_description,
+            color=_color
+        )
+
+        if "fields" in kwargs:
+            for field in kwargs["fields"]:
+                embed.add_field(
+                    name=field["name"],
+                    value=field["value"],
+                    inline=field["inline"] if "inline" in field else False
+                )
+
+        self.number_emojis = (kwargs["emojis"] if "emojis" in kwargs else global_values.number_emojis)[:len(_choices)]
+        self.number_emojis.append(kwargs["validation_emoji"] if "validation_emoji" in kwargs else "✅")
+
+        if "silent" not in kwargs:
+            embed.description += '\n' + '\n'.join([self.number_emojis[i] + " " + x for i, x in enumerate(_choices)])
+
+        self.message = await _channel.send(embed=embed)
+
+        for i, _ in enumerate(_choices):
+            await self.message.add_reaction(self.number_emojis[i])
+
+        global_values.reaction_messages.append(self)
+
+    # Trigger quand une réaction est ajoutée
+    async def add_reaction(self, reaction, user):
+        print("add " + str(user.id))
+        if user.id in self.reactions:
+            if reaction.emoji != self.number_emojis[-1]:
+                self.reactions[user.id].append(self.number_emojis.index(reaction.emoji))
+        else:
+            self.reactions[user.id] = [self.number_emojis.index(reaction.emoji)]
+
+        condition_on = await self.cond(self.reactions)
+        if reaction.emoji == self.number_emojis[-1] and condition_on and not self.block:
+            self.block = True
+            await self.effect(self.reactions)
+
+            if self.temporary:
+                await self.message.delete()
+
+            global_values.reaction_messages.remove(self)
+        else:
+            await self.update(reaction)
+
+    # Trigger quand une réaction est retirée
+    async def remove_reaction(self, reaction, user):
+        print("remove " + str(user.id))
+        self.reactions[user.id].remove(self.number_emojis.index(reaction.emoji))
+
+        # if len(self.reactions[user.id]) == 0:
+        #     self.reactions.pop(user.id)
+
+        await self.update(reaction)
+
+    # Vérifie si la coche doit être affichée et fait la fonction update_function si elle existe
+    async def update(self, reaction):
+        print("UPDATE")
+        print(self.reactions)
+
+        if self.update_function:
+            await self.update_function(self.reactions)
+
+        if await self.cond(self.reactions):
+            print("Try and add")
+            await self.message.add_reaction(self.number_emojis[-1])
+        else:
+            if reaction.message.guild:
+                await self.message.remove_reaction(self.number_emojis[-1], reaction.message.guild.me)
+            else:
+                await self.message.remove_reaction(self.number_emojis[-1], reaction.message.channel.me)
