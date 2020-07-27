de2fd7f5e8314e3716ea698a52e571b7869a73b6
Le Codex <40335983+LeCodex@users.noreply.github.com>
2020-07-27 22:03:28 +0200

    [mod-petri] Changed arrows' order and added last choice indication

--- modules/petri/game.py
+++ modules/petri/game.py
@@ -29,6 +29,7 @@ class Game:
         self.ranges = [10, 10, 2]  # Taille horizontale, taille verticale, nombre de murs par quartiers
         self.info_message = None
         self.game_creation_message = None
+        self.last_choice = ""
 
     async def reload(self, object, client):
         await self.deserialize(object, client)
@@ -130,7 +131,7 @@ class Game:
         fields = []
 
         fields.append({
-            "name": "Joueurs (Score de Domination: " + str(int((self.ranges[0] * self.ranges[1])/2)) +  ")",
+            "name": "Joueurs (Score de Domination : " + str(int((self.ranges[0] * self.ranges[1])/2)) +  ")" + (" / Dernière direction choisie : " + global_values.choice_emojis[self.last_choice] if len(self.last_choice) else ""),
             "value":'\n'.join([global_values.tile_colors[i + 2] + " `" + str(self.players[x].user) + "` : " + str(len([0 for row in self.map for tile in row if tile == i])) for i, x in enumerate(self.order)])
         })
 
@@ -180,6 +181,7 @@ class Game:
                                 for i in reactions[self.order[self.turn]]:
                                     await self.info_message.message.remove_reaction(global_values.choice_emojis[i], self.players[self.order[self.turn]].user)
                                 self.info_message.reactions = {}
+                                self.last_choice = 4
 
                                 await self.next_turn({
                                     "name": "Capitulation",
@@ -193,6 +195,7 @@ class Game:
                             for i in reactions[self.order[self.turn]]:
                                 await self.info_message.message.remove_reaction(global_values.choice_emojis[i], self.players[self.order[self.turn]].user)
                             self.info_message.reactions = {}
+                            self.last_choice = reactions[self.order[self.turn]][0]
 
                             await self.next_turn({
                                 "name": "Combats",
@@ -215,7 +218,7 @@ class Game:
                 "[PETRI Manche " + str(self.round) + "] Tour de `" + str(self.players[self.order[self.turn]].user) + "`",
                 map_string,
                 global_values.player_colors[self.turn],
-                ["Haut", "Droite", "Bas", "Gauche", "Capituler", "Valider"],
+                ["Gauche", "Haut", "Bas", "Droite", "Capituler", "Valider"],
                 emojis=global_values.choice_emojis,
                 silent=True,
                 fields=fields
@@ -226,8 +229,8 @@ class Game:
 
     #Gère les combats et les réplications
     def process_direction(self, choice):
-        dx = [0, 1, 0 , -1][choice]
-        dy = [-1, 0, 1 , 0][choice]
+        dx = [-1, 0, 0 , 1][choice]
+        dy = [0, -1, 1 , 0][choice]
         new_map = copy.deepcopy(self.map)
         summary = []
 
