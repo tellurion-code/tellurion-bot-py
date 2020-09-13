b17638c32feba7466b7171275e18063d0c5b8214
Louis Chauvet <louis.chauvet@free.fr>
2020-09-13 16:18:50 +0200

    Create __init__.py

---
+++ modules/carte/__init__.py
@@ -0,0 +1,24 @@
+import random
+import discord
+
+from modules.base import BaseClassPython
+
+NUMEROS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "valet", "dame", "roi"]
+COULEURS = ["coeur", "pique", "trèfle", "carreau"]
+
+class MainClass(BaseClassPython):
+    name = "card"
+    help = {
+        "description": "Tire une carte",
+        "commands": {
+            "{prefix}{command}": "Tire une carte"
+        }
+    }
+
+    def __init__(self, client):
+        super().__init__(client)
+
+    async def command(self, message, args, kwargs):
+        couleur = random.choice(COULEUR)
+        numero = random.choice(NUMERO)
+        await message.channel.send(f"Vous avez tiré la carte: {numero} de {couleur}")
