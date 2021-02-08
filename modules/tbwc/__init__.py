8dbbe7af7887bd6477abb5d91afbf137332f9a9d
Le Codex <40335983+LeCodex@users.noreply.github.com>
2021-02-08 14:30:27 +0100

    [mod-tbwc] Added admin, author, editor and made a better reshuffle

--- modules/tbwc/__init__.py
+++ modules/tbwc/__init__.py
@@ -34,12 +34,13 @@ class MainClass(BaseClassPython):
 			msg = None
 
 			if not game["list"][index]:
-				msg = await message.channel.send("```md\n" + str(index + 1) + ". [Carte Blanche] Envoyez un message pour définir la carte en tapant \"[Nom de la carte](Type): Effet\"! (Type) accepte Aura, Terrain ou Ephémère.```")
+				msg = await message.channel.send("```md\n" + str(index + 1) + ". [Carte Blanche] Envoyez un message pour définir la carte en tapant \"[Nom de la carte](Type) Effet\"! (Type) accepte Aura, Terrain ou Ephémère.```")
 				await self.startCardCreation(message.author, message.channel, index)
 				# await message.channel.send("Carte définie")
+				game["list"][index]["author"] = message.author.display_name
 
 			card = game["list"][index]
-			content = "```md\n" + str(index + 1) + ". " + self.printCard(card) + "```"
+			content = "```md\n" + str(index + 1) + ". " + self.printCard(card) + " " + self.printCardAuthors(card) + "```"
 			if msg:
 				await msg.edit(content=content)
 			else:
@@ -47,10 +48,10 @@ class MainClass(BaseClassPython):
 
 			game["discard"].append(index)
 
-			if len(game["deck"]) <= len(game["list"])/5:
-				random.shuffle(game["discard"])
-				game["deck"].extend(game["discard"])
+			if len(game["deck"]) <= len(game["list"])/4:
 				game["discard"].clear()
+				game["deck"] = [i for i in range(len(game["list"]))]
+				random.shuffle(game["deck"])
 
 				await message.channel.send("```http\nLe deck est trop petit! La défausse a été mélangée pour reformer un nouveau deck```")
 
@@ -70,14 +71,15 @@ class MainClass(BaseClassPython):
 	async def com_start(self, message, args, kwargs):
 		if str(message.channel.id) in self.games:
 			await message.channel.send("Il y a déjà une partie en cours dans ce salon")
-		else:
+		elif message.guild:
 			if len(args) > 1:
 				try:
 					amount = int(args[1])
 					game = {
 						"deck": [i for i in range(amount)],
 						"discard": [],
-						"list": [None for i in range(amount)]
+						"list": [None for i in range(amount)],
+						"admin": message.author.id
 					}
 
 					random.shuffle(game["deck"])
@@ -95,20 +97,26 @@ class MainClass(BaseClassPython):
 		if str(message.channel.id) in self.games:
 			game = self.games[str(message.channel.id)]
 			if len(args) > 1:
+				index = None
 				try:
-					index = int(args[1])
-
-					if index < 1 or index > len(game["list"]):
-						await message.channel.send("Aucune carte n'a cet index")
-					else:
-						await message.channel.send("Définissez la carte en tapant \"[Nom de la carte](Type): Effet\". (Type) accepte Aura, Terrain ou Ephémère.")
-						await self.startCardCreation(message.author, message.channel, index - 1)
-						# await message.channel.send("Carte éditée")
-						await self.sendCardList(message.channel)
-
-					self.objects.save_object("games", self.games)
+					index = int(args[1]) - 1
 				except:
 					await message.channel.send("Veuillez renseigner l'index de la carte à modifier")
+
+				if index:
+					if index < 0 or index >= len(game["list"]):
+						await message.channel.send("Aucune carte n'a cet index")
+					else:
+						msg = await message.channel.send("Définissez la carte en tapant \"[Nom de la carte](Type): Effet\". (Type) accepte Aura, Terrain ou Ephémère.")
+						author=game["list"][index]["author"]
+
+						await self.startCardCreation(message.author, message.channel, index)
+						game["list"][index]["editor"] = message.author.display_name
+						game["list"][index]["author"] = author
+						# await message.channel.send("Carte éditée")
+						await msg.edit(content="```md\n" + str(index + 1) + ". " + self.printCard(game["list"][index]) + " " + self.printCardAuthors(game["list"][index]) + "```")
+
+					self.objects.save_object("games", self.games)
 			else:
 				await message.channel.send("Veuillez renseigner l'index de la carte à modifier")
 		else:
@@ -116,11 +124,15 @@ class MainClass(BaseClassPython):
 
 	async def com_end(self, message, args, kwargs):
 		if str(message.channel.id) in self.games:
-			await self.sendCardList(message.channel)
-			del self.games[str(message.channel.id)]
-			await message.channel.send("Partie finie!")
+			game = self.games[str(message.channel.id)]
+			if (game["admin"] == message.author.id):
+				await self.sendCardList(message.channel)
+				del self.games[str(message.channel.id)]
+				await message.channel.send("Partie finie!")
 
-			self.objects.save_object("games", self.games)
+				self.objects.save_object("games", self.games)
+			else:
+				await message.channel.send("Vous n'avez pas le droit d'exécuter cette commande. Seul l'administrateur de la partie peut l'arrêter")
 		else:
 			await message.channel.send("Aucune partie n'est actuellement en cours dans ce salon")
 
@@ -153,11 +165,14 @@ class MainClass(BaseClassPython):
 		match = re.match(regex, msg.content)
 
 		card = {
-		"name": match.groups()[0],
-		"type": match.groups()[1],
-		"effect": match.groups()[2]
+			"name": match.groups()[0],
+			"type": match.groups()[1],
+			"effect": match.groups()[2]
 		}
 		self.games[str(channel.id)]["list"][index] = card
 
 	def printCard(self, card):
 		return "[" + card["name"] +"](" + card["type"] + ") " + card["effect"]
+
+	def printCardAuthors(self, card):
+		return "(Créée par " + card["author"] + (", éditée par " + card["editor"] if "editor" in card else "") + ")"
