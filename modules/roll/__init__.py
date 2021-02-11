ef0ad4beb7f16557a898ad61bcf929cc8d40d766
Alix Eisenhardt <alix.vuagnoux@gmail.com>
2021-02-11 02:38:03 +0100

    Add some *random* stuff

--- modules/roll/__init__.py
+++ modules/roll/__init__.py
@@ -35,7 +35,7 @@ class MainClass(BaseClassPython):
 		amount = 6
 		if len(args) == 1:
 			try:
-				amount = int(args)
+				amount = int(args[0])
 			except:
 				await message.channel.send("Un des arguments n'est pas un nombre valide")
 				return
