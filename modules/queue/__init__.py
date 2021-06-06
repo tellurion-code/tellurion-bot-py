fd269c7ffd612045159bb3856b8514f23553c87c
Le Codex <40335983+LeCodex@users.noreply.github.com>
2021-06-06 21:57:13 +0200

    [mod-queue] Initial commit

---
+++ modules/queue/__init__.py
@@ -0,0 +1,60 @@
+import discord
+
+from modules.base import BaseClassPython
+
+QUEUE = [];
+
+class MainClass(BaseClassPython):
+	name = "queue"
+	help = {
+		"description": "Gère une queue",
+		"commands": {
+			"{prefix}{command}": "Envoie la liste",
+			"{prefix}{command} add": "S'ajouter à la liste",
+			"{preifx}{command} remove": "S'enlever de la liste",
+			"{preifx}{command} nest": "Ping le suivant dans la liste",
+		}
+	}
+
+	def __init__(self, client):
+		super().__init__(client)
+		self.config["auth_everyone"] = True
+		self.config["configured"] = True
+
+
+	async def command(self, message, args, kwargs):
+		if len(QUEUE) == 0:
+			await message.channel.send("Queue vide")
+			return
+
+		await message.channel.send(embed=discord.Embed(
+			description='\n'.join([str(x) for x in QUEUE])
+		))
+
+	async def com_add(self, message, args, kwargs):
+		QUEUE.append(message.author)
+
+		await message.channel.send(embed=discord.Embed(
+			description='\n'.join([str(x) for x in QUEUE])
+		))
+
+	async def com_remove(self, message, args, kwargs):
+		QUEUE.remove(message.author)
+
+		if len(QUEUE) == 0:
+			await message.channel.send("Queue vide")
+			return
+
+		await message.channel.send(embed=discord.Embed(
+			description='\n'.join([str(x) for x in QUEUE])
+		))
+
+	async def com_next(self, message, args, kwargs):
+		if len(QUEUE) == 0:
+			await message.channel.send("Queue vide")
+			return
+
+
+		next = QUEUE.pop(0)
+
+		await message.channel.send(next.mention)
