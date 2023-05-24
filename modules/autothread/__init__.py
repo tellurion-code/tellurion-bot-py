3e021c9fc8f0a9a1854196a1b2763144550e58b4
Kernel <cysteic@protonmail.com>
2023-05-24 18:47:57 +0200

    [mod-autothread] Clean command and active channels save
    Now it works across restarts. I also added a clean command
    to get rid of all 'empty' threads in a given channel.

--- modules/autothread/__init__.py
+++ modules/autothread/__init__.py
@@ -9,36 +9,37 @@ class MainClass(BaseClassPython):
 		"commands": {
 			"{prefix}{command} enable": "Active la création automatique de threads dans le channel actuel",
 			"{prefix}{command} disable": "Désactive la création automatique de threads",
-			"{prefix}{command} list": "Affiche la liste des channels actifs (debug)"
+			"{prefix}{command} list": "Affiche la liste des salons enregistrés dans le module",
+			"{prefix}{command} clean": "Supprime tous les threads 'vides' (avec un seul message) du salon"
 	}}
 	active_channels = []
 
 	def __init__(self, client):
 		super().__init__(client)
-		self.config["auth_everyone"] = True
-		self.config["authorized_roles"] = []
+		self.config["auth_everyone"] = False
 		self.config["configured"] = True
 		self.config["help_active"] = True
 		self.config["command_text"] = "autothread"
-		self.config["color"] = 0x57ab1e
+		self.config["color"] = 0x840052 # Kernel's unique color
 		
 	async def on_ready(self):
-		#if self.objects.save_exists("active_channels"):
-		#	self.active_channels = self.objects.load_object("active_channels")
-		pass
+		if self.objects.save_exists("active_channels"):
+			self.active_channels = self.objects.load_object("active_channels")
 	
 	async def on_message(self, message):
 		await super().on_message(message)
+		
+		# Ignoring bot messages and commands
 		if message.author.bot or message.content.startswith(self.client.config["prefix"]):
-			# print("autothread: ignoring command/bot message")
 			return
+			
 		if str(message.channel.id) in self.active_channels:
-			await message.create_thread(name=f"Thread {message.author.name}-{randrange(18072)}")
+			await message.create_thread(name=f"Thread {message.author.name}-{randrange(1807)}")
 	
 	async def com_enable(self, message, args, kwargs):
 		if str(message.channel.id) not in self.active_channels:
 			self.active_channels.append(str(message.channel.id))
-			#self.save_channels()
+			self.saveChannels()
 			await message.channel.send("Salon ajouté.")
 		else:
 			await message.channel.send("AutoThread est déjà actif dans ce salon.")
@@ -46,7 +47,7 @@ class MainClass(BaseClassPython):
 	async def com_disable(self, message, args, kwargs):
 		if str(message.channel.id) in self.active_channels:
 			self.active_channels.remove(str(message.channel.id))
-			#self.save_channels()
+			self.saveChannels()
 			await message.channel.send("Salon retiré.")
 		else:
 			await message.channel.send("Le salon n'est pas enregistré dans AutoThread.")
@@ -58,8 +59,22 @@ class MainClass(BaseClassPython):
 			channel_list += "<#{0}>\n".format(chan_id)
 			
 		await message.channel.send(f"`autothread` | **Salons enregistrés ({len(self.active_channels)})** \n" + channel_list)
+	
+	async def com_clean(self, message, args, kwargs):
+		if str(message.channel.id) in self.active_channels:
+			deleted_threads = 0
+			for thread in message.channel.threads:
+				# Deleting threads only if they have one message
+				messages = [msg async for msg in thread.history(limit=5)]
+				if not len(messages) >= 2:
+					deleted_threads += 1
+					await thread.delete()
+					
+			await message.channel.send(f"`autothread clean` | Threads supprimés : {deleted_threads}")
+		else:
+			await message.channel.send("Le salon n'est pas enregistré dans AutoThread.")
 		
 
-	#def save_channels(self):
-	#	self.mainclass.objects.save_object("active_channels", self.active_channels)
+	def saveChannels(self):
+		self.objects.save_object("active_channels", self.active_channels)
 	
