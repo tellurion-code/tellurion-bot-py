1e7716e35bc9435c6572b2f37a2a5389fb0c04e3
Alix Eisenhardt <Alix-Eisenhardt@users.noreply.github.com>
2025-03-07 21:55:44 +0100

    secret

---
+++ modules/secret/__init__.py
@@ -0,0 +1,44 @@
+import random
+import discord
+from modules.base import BaseClassPython
+
+def userstr(user):
+    if user:
+        return user.name
+    else:
+        return "None"
+
+class MainClass(BaseClassPython):
+    name = "Secret"
+    help = {
+        "description": "Secret",
+        "commands": {
+            "There is no commands"
+        }
+    }
+    games = {}
+
+    colors = []
+
+    def __init__(self, client):
+        super().__init__(client)
+        self.config["auth_everyone"] = True
+        self.config["configured"] = True
+        self.config["help_active"] = True
+        self.config["command_text"] = "roll"
+        self.config["color"] = 0x000000
+
+    async def on_ready(self):
+        self.change_color.start()
+
+    @discord.ext.task.loop(seconds=10)
+    async def change_color(self):
+        guild = self.client.get_guild(self.client.config.main_guild)
+        role = guild.get_role(1347659937542574140)
+        if role:
+            new_color = self.colors.pop(0)
+            await role.edit(color=new_color)
+
+    @change_color.before_loop
+    async def before_loop():
+        await discord.commands.bot.wait_until_ready()
